b1d06ea2b5519980a3446be156538aff862558fa
Gugu Mtonjeni <gammtonjeni@gmail.com>
2023-05-08 21:23:24 -0700

    Mandantory tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to the POSIX.
+ * read_textfile- Read text file, print to POSIX STDOUT.
  * @filename: Text file being read
  * @letters: Number of letters to be read
- * Return: w- Actual number of bytes read and printed
- *        0- When function fails or filename is NULL.
+ * Return: w - Actual number of bytes read and printed
+ *        0 - When function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        char *buf;
-        ssize_t fd;
-        ssize_t w;
-        ssize_t t;
+	char *buf;
+	ssize_t o;
+	ssize_t w;
+	ssize_t r;
 
-        o = open(filename, O_RDONLY);
-        if (o == -1)
-                return (0);
-        buf = malloc(sizeof(char) * letters);
-        t = read(o, buf, letters);
-        w = write(STDOUT_FILENO, buf, t);
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-        free(buf);
-        close(o);
-        return (w);
+	free(buf);
+	close(o);
+	return (w);
 }
